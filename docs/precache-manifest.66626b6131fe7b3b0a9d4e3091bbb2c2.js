15e72997106644ba97492d817a20adcb6545c114
James <jamalkiewicz@gmail.com>
2021-01-21 22:26:02 -0800

    template

---
+++ docs/precache-manifest.66626b6131fe7b3b0a9d4e3091bbb2c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdf12b0481958941593c66793427325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eb7889dc6b23ff6eef6",
+    "url": "/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "ca0450e4f30c1e9a09de",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "9eb7889dc6b23ff6eef6",
+    "url": "/static/js/2.c9aa383a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c9aa383a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2fa200065c224fbccb",
+    "url": "/static/js/3.201c2ecc.chunk.js"
+  },
+  {
+    "revision": "ca0450e4f30c1e9a09de",
+    "url": "/static/js/main.afff6adb.chunk.js"
+  },
+  {
+    "revision": "f2032049fb3b0b92f1fb",
+    "url": "/static/js/runtime-main.2fdbf97e.js"
+  }
+]);
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Needs tools
+
+Visual Studio Code
+NPM
+
+# To create project from scratch
+
+npm install -g create-react-app  
+npx create-react-app my-app  
+cd my-app  
+npm start
+
+# Command Cheatsheet
+
+build: npm run-script build
+run: npm run-script start
